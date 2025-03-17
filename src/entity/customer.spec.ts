87b832adcbff8c393b1da6064d06c32eb1f90164
pauloggdev <pauloggjoao@gmail.com>
2025-03-17 22:16:52 +0100

    add file test

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -2,6 +2,46 @@ import Address from "./address";
 import Customer from "./customer";
 
 describe("Customer unit tests", () => {
+    it('should throw error when id is empty', ()=>{
+        expect(()=>{
+            let customer = new Customer("", "John")
+        }).toThrowError('ID cannot be empty')
+    })
+    it('should throw error when name is empty', ()=>{
+        expect(()=>{
+            let customer = new Customer("123", "")
+        }).toThrowError('Name cannot be empty')
+    })
+    it("should update name an customer", () =>{
+        let customer = new Customer("123", "John");
+        customer.updateName("John Doe");
+        expect(customer._name).toBe("John Doe");
+    })
+    it("should update address an customer", () =>{
+        let address = new Address("street 2", "City 2", "State 2", "12234")
+        let customer = new Customer("123", "John", address);
+        let address2 = new Address("Street 3", "City 3", "State 3", "12234")
+        customer.updateAddress(address2);
+        expect(customer._address?._city).toBe("City 3");
+        expect(customer._address?._street).toBe("Street 3");
+        expect(customer._address?._state).toBe("State 3");
+        expect(customer._address?._zipCode).toBe("12234");
+    })
+    it("should return all address", () =>{
+        let address = new Address("street 2", "City 2", "State 2", "12234")
+        let customer = new Customer("123", "John", address);
+        expect(customer.getAddress()).toBe(address);
+    })
+    it("should activate an customer", () =>{
+        let customer = new Customer("123", "John");
+        customer.activate();
+        expect(customer._active).toBeTruthy()
+    })
+    it("should deactivate an customer", () =>{
+        let customer = new Customer("123", "John");
+        customer.deactivate();
+        expect(customer._active).toBeFalsy()
+    })
     it("should create a new customer", () => {
         // Arrange
         const id = "123";
--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,33 +1,37 @@
 import Address from "./address";
 
-export default class Customer{
+export default class Customer {
 
     _id: string;
     _name: string;
     _address?: Address;
     _active: boolean = true;
-    
-    constructor(id:string, name:string, address?:Address){
+
+    constructor(id: string, name: string, address?: Address) {
         this._id = id;
         this._name = name;
         this._address = address;
         this.validate();
     }
-    validate(){
-        if(this._name.trim().length === 0){
-            throw new Error("Name cannot be empty");
-        }
-        if(this._id.trim().length === 0){
+    validate() {
+        if (this._id.trim().length === 0) {
             throw new Error("ID cannot be empty");
         }
+        if (this._name.trim().length === 0) {
+            throw new Error("Name cannot be empty");
+        }
+
     }
-    updateName(name:string){
+    updateName(name: string) {
         this._name = name;
     }
-    updateAddress(address:Address){
+    updateAddress(address: Address) {
         this._address = address;
     }
-    activate(){
+    getAddress(): Address | undefined {
+        return this._address;
+    }
+    activate() {
         this._active = true;
     }
     deactivate() {
